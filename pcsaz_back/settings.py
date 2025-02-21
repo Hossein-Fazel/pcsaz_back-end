aaaf6bb23d35233976139617376bf1ef3489e4dd
Hossein Fazel <fazel.hosein9654@gmail.com>
2025-02-21 21:03:09 +0330

    change base settings of server

--- pcsaz_back/settings.py
+++ pcsaz_back/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&ik3jf@rlf@76#8l)grs9o_rvs1ons3z$*$s0aatbe480$r@p8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
