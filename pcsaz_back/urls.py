9d5f6b9fc8a084ef84f2d2406de523ba5cca99ed
Hossein Fazel <fazel.hosein9654@gmail.com>
2025-02-22 13:12:24 +0330

    change base urls

--- pcsaz_back/urls.py
+++ pcsaz_back/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', login, name='login'),
-    path('personal_data/', get_personal, name='get_peersonal'),
-    path('vip_detail/', get_vip_detail, name='vip_detail'),
-    path('discount_detail/', get_discount_detail, name='discount_detail')
+    path('user/', include('user.urls') , name='login')
 ]
---
+++ user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path("login/", login, name="login"),
+    path('personal_data/', get_personal, name='get_peersonal'),
+    path('vip_detail/', get_vip_detail, name='vip_detail'),
+    path('discount_detail/', get_discount_detail, name='discount_detail')
+]
